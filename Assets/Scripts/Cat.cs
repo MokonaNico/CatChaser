fd7105275b3c6536467ae5968f4a1d865f6521ec
MokonaNico <mine-nico@hotmail.com>
2021-08-17 19:32:05 +0200

    push project

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    
+    public float speed = 10;
+    public GameObject fish;
+    public GameHandler gameHandler;
+    public int value = 10;
+
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+
+    private bool clicked = false;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        endPosition = fish.transform.position;
+    }
+
+
+    void Update()
+    {
+        if (!gameHandler.gameIsOn)
+            return;
+        
+        if (!clicked)
+        {
+            if (transform.position != endPosition)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, endPosition, speed * Time.deltaTime);
+            }
+
+            else
+            {
+                gameHandler.Defeat();
+            }
+        }
+        else{
+            transform.position = Vector3.MoveTowards(transform.position, startPosition, (speed + 2) * Time.deltaTime);
+            if (transform.position == startPosition)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!clicked && gameHandler.gameIsOn)
+        {
+            clicked = true;
+            gameHandler.increaseScore(value);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameHandler : MonoBehaviour
+{
+    
+    public bool gameIsOn = true;
+    public GameObject catPrefab;
+    public GameObject fish;
+    private int score;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject restartButton;
+    public GameObject scorePanel;
+    public GameObject submitScoreButton;
+    public GameObject scoreInputField;
+
+    public float catSpeed = 2f;
+    public float spawnTime = 1f;
+    public float difficultyIncreaseTime = 0.1f;
+    private float x = 0;
+
+    void Start()
+    {
+        refreshScoreText();
+        playButton.GetComponent<Button>().onClick.AddListener(StartGame);
+        playButton.SetActive(true);
+        
+        scorePanel.SetActive(false);
+        submitScoreButton.GetComponent<Button>().onClick.AddListener(SubmitScore);
+        
+        restartButton.GetComponent<Button>().onClick.AddListener(RestartGame);
+        restartButton.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(SpawnCatsCoroutine());
+        InvokeRepeating("increaseDifficulty", 0, difficultyIncreaseTime);
+        playButton.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    public void Defeat()
+    {
+        gameIsOn = false;
+        scorePanel.SetActive(true);
+        
+    }
+
+    private void SubmitScore()
+    {
+        NetworkHandler networkHandler = new NetworkHandler();
+        string playerName = scoreInputField.GetComponent<InputField>().text;
+        StartCoroutine(networkHandler.UploadScore(playerName,score));
+        restartButton.SetActive(true);
+        scorePanel.SetActive(false);
+    }
+    
+    private Vector3 GetRandomVect()
+    {
+        if (Camera.main == null) return Vector3.zero;
+        Vector3 stageDimension = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        int delta = Random.Range(2,8);
+        Vector2 topRight = new Vector2(stageDimension.x + delta, stageDimension.y + delta);
+        Vector2 topLeft = new Vector2(-stageDimension.x - delta, stageDimension.y + delta);
+        Vector2 bottomRight = new Vector2(stageDimension.x + delta, -stageDimension.y - delta);
+        Vector2 bottomLeft = new Vector2(-stageDimension.x - delta, -stageDimension.y - delta);
+
+        Vector3 spawnPoint = Vector3.zero;
+        int zoneDecision = Random.Range(0, 4);
+        
+        switch (zoneDecision)
+        {
+            case 0:
+                spawnPoint = new Vector3(topLeft.x, Random.Range(topLeft.y, bottomLeft.y),0);
+                break;
+            case 1:
+                spawnPoint = new Vector3(Random.Range(topLeft.x, topRight.x), topLeft.y,0);
+                break;
+            case 2:
+                spawnPoint = new Vector3(topRight.x, Random.Range(topRight.y, bottomRight.y),0);
+                break;
+            case 3:
+                spawnPoint = new Vector3(Random.Range(bottomLeft.x, bottomRight.x), bottomLeft.y,0);
+                break;
+        }
+        return spawnPoint;
+    }
+
+    private void GenerateCat()
+    {
+        GameObject catGameObject = Instantiate(catPrefab, GetRandomVect(),Quaternion.identity);
+        Cat cat = catGameObject.GetComponent<Cat>();
+        cat.fish = fish;
+        cat.speed = catSpeed;
+        cat.gameHandler = this;
+    }
+
+    private IEnumerator SpawnCatsCoroutine()
+    {
+        while (gameIsOn)
+        {
+            GenerateCat();
+            yield return new WaitForSeconds(spawnTime);         
+        }
+    }
+
+    public void increaseScore(int addScore)
+    {
+        score += addScore;
+        refreshScoreText();
+
+    }
+
+    private void refreshScoreText()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    private void increaseDifficulty()
+    {
+        x += difficultyIncreaseTime;
+        catSpeed = (1 / (1 + Mathf.Exp(-0.03f * x + 2.5f))) * 2.5f + 2;
+        spawnTime = (1 / (1 + Mathf.Exp(-(-0.03f) * x - 3.3f))) * 0.7f + 0.3f;
+    }
+}
